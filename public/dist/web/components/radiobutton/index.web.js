c862055938ecbb286420846f431acb0708bdf8cc
JaneLMF <long.meifeng@pivosgroup.com.cn>
2017-07-27 10:15:41 +0800

    modify dist for 91nn

--- public/dist/web/components/radiobutton/index.web.js
+++
@@ -1,3 +0,0 @@
-// { "framework": "Vue"} 
-
-!function(r){function e(o){if(n[o])return n[o].exports;var t=n[o]={i:o,l:!1,exports:{}};return r[o].call(t.exports,t,t.exports,e),t.l=!0,t.exports}var n={};e.m=r,e.c=n,e.i=function(r){return r},e.d=function(r,n,o){e.o(r,n)||Object.defineProperty(r,n,{configurable:!1,enumerable:!0,get:o})},e.n=function(r){var n=r&&r.__esModule?function(){return r.default}:function(){return r};return e.d(n,"a",n),n},e.o=function(r,e){return Object.prototype.hasOwnProperty.call(r,e)},e.p="",e(e.s=30)}({19:function(r,e){throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/Users/jane/MyWorld/Work/node/weex-91nn/src/components/radiobutton/index.vue'")},30:function(r,e,n){"use strict";var o=n(19);o.el="#root",new Vue(o)}});
